1ca31e6977a576f20cf0b5f03cc243a84957de72
Palagiri Chenna Kesava Reddy <chennakesava476@gmail.com>
2025-05-03 22:03:10 +0530

    Added HTML interface to Flask app

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import recommendation_engine  # your logic script
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route('/')
-def home():
-    return "Hello from Render!"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    recommendations = None
+    if request.method == "POST":
+        query = request.form["query"]
+        recommendations = recommendation_engine.get_recommendations(query)
+    return render_template("index.html", recommendations=recommendations)
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))  # Use Render's provided PORT
-    app.run(host='0.0.0.0', port=port)
+if _name_ == "_main_":
+    app.run(debug=True)
